2077d54b17b3033c5e79fb3b1e55a21a27515d07
Behzod <okilovbehzod8@gmail.com>
2023-11-22 18:30:48 +0500

    create fille main.py

---
+++ Assignment6/main.py
@@ -0,0 +1,16 @@
+#Code 1
+num1 = 15.5
+num2 = 8
+print(type(num1))
+print(type(num2))
+print((float(num2)) * num1)
+print(num1+num2)
+
+
+a = "Hello, Python learnes"
+print(len(a))
+print(a[7 : 13])
+print(a)
+# Len()- baroi darozii khatro guftan ba 
+print(a.lower())
+#Lower() tamomi harfhoro khurd nishon medihad
